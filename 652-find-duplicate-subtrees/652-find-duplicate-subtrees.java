425e3cbf32dc18a207d33852ca933d5dd87c276a
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-02-27 17:46:13 +0530

    Time: 32 ms (59.98%), Space: 54.9 MB (36.37%) - LeetHub

---
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<String, Integer> hm = new HashMap<>();
+    List<TreeNode> result = new ArrayList<>();
+    
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        if(root == null) return result;
+        preSerialize(root);
+        return result;
+    }
+    
+    public String preSerialize(TreeNode root){
+        if(root == null) return "#,";
+        
+        String serial = root.val + "," + preSerialize(root.left) + preSerialize(root.right);
+        if(hm.getOrDefault(serial, 0) == 0){
+            hm.put(serial, 1);
+        } else {
+            if(hm.get(serial)==1){
+                result.add(root);
+            }
+            hm.put(serial, hm.get(serial) + 1);
+        }
+        
+        return serial;
+    }
+}
