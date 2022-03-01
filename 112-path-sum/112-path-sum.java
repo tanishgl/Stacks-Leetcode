6c257c4df86bdbdbf3d22e5db39e821166470dfc
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-03-01 20:42:21 +0530

    Time: 0 ms (100.00%), Space: 44 MB (12.75%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -14,34 +14,11 @@
  * }
  */
 class Solution {
-    
-//     boolean flag = false;
-    
-//     public void helper(TreeNode root, int target){
-//         if(root==null) return;
-        
-//         if(root.left==null && root.right==null){
-//             flag = flag || (root.val == target);
-//             return;
-//         }
-        
-//         helper(root.left, target - root.val);
-//         helper(root.right, target - root.val);
-//     }
-    
-//     public boolean hasPathSum(TreeNode root, int targetSum) {
-//         if(root==null) return false;
-//         helper(root, targetSum);
-//         return flag;
-//     }
-    
-    public boolean hasPathSum(TreeNode root, int targetSum){
+    public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null) return false;
         
-        if(root.left==null && root.right==null){
-            //leaf node.
-            return root.val==targetSum;
-        }
+        if(root.left==null && root.right==null)
+            return root.val == targetSum;
         
         return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
     }
