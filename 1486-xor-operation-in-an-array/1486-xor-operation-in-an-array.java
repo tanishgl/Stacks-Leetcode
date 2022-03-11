be66864c39aee6ff124aaf2dfb65c5928ad41f77
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-03-12 00:26:34 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (56.31%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int res = 0;
+        
+        for(int i=0;i<n;i++){
+            int val = start + (2*i);
+            res ^= val;
+        }
+        
+        return res;
+    }
+}
