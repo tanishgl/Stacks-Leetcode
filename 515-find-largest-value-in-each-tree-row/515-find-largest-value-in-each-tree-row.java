687be437ac91c91efea928e8a931d8f009325d0c
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-03-11 23:42:59 +0530

    Time: 1 ms (96.95%), Space: 42.6 MB (57.89%) - LeetHub

---
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        
+        while(q.size()>0){
+            int size = q.size();
+            int max = Integer.MIN_VALUE;
+            while(size-->0){
+                TreeNode curr = q.remove();
+                if(curr.val > max)
+                    max = curr.val;
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+            }
+            res.add(max);
+        }
+        
+        return res;
+    }
+}
