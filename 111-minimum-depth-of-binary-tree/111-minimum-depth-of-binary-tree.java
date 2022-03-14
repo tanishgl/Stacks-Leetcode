22c46e00d22eb720c9f2afc79d9cdffb9fbf7526
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-03-14 23:13:50 +0530

    Time: 1 ms (95.76%), Space: 95.2 MB (24.12%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -15,21 +15,40 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        if(root==null){
-            //Tree is null. So, no depth -> 0 depth.
-            return 0;
-        } else if (root.left==null && root.right==null){
-            //Leaf Node.
-            return 1;
-        } else if (root.left==null){
-            //Only right child exists.
-            return 1 + minDepth(root.right);
-        } else if (root.right==null){
-            //Only left child exists.
-            return 1 + minDepth(root.left);
-        } else {
-            //2 - child node.
-            return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        // if(root==null){
+        //     //Tree is null. So, no depth -> 0 depth.
+        //     return 0;
+        // } else if (root.left==null && root.right==null){
+        //     //Leaf Node.
+        //     return 1;
+        // } else if (root.left==null){
+        //     //Only right child exists.
+        //     return 1 + minDepth(root.right);
+        // } else if (root.right==null){
+        //     //Only left child exists.
+        //     return 1 + minDepth(root.left);
+        // } else {
+        //     //2 - child node.
+        //     return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        // }
+        
+        if(root == null) return 0;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        
+        int level = 1;
+        
+        while(q.size()>0){
+            int size = q.size();
+            while(size-->0){
+                TreeNode curr = q.remove();
+                if(curr.left == null && curr.right == null) return level;
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+            }
+            level++;
         }
+        
+        return level;
     }
 }
