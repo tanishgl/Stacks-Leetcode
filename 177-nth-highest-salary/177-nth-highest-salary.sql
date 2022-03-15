35acb64189b76b729010dcda6c870bd3caa5579c
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-03-16 00:08:19 +0530

    Time: 321 ms (69.88%), Space: 0B (100.00%) - LeetHub

---
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      Select ifnull(
+      (
+          select distinct salary from employee
+          order by salary desc
+          limit N, 1
+      ), null)
+  );
+END
