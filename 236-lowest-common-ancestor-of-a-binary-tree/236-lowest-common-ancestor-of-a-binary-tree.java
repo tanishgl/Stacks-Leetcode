581f40f94f13074e4c7724d9ebc4c28c2ccbdac8
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-03-16 22:42:56 +0530

    Time: 12 ms (35.64%), Space: 47.2 MB (47.67%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,47 +9,22 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(p.left == q || p.right == q) return p;
-        if(q.left == p || q.right == p) return q;
-        ArrayList<Integer> a1 = n2rpath(root, p.val);
-        ArrayList<Integer> a2 = n2rpath(root, q.val);
+        if(root == null) return null;
         
-        int lca = 0;
-        
-        int i = a1.size()-1, j = a2.size()-1;
-        
-        while(i>=0 && j>=0 && a1.get(i)==a2.get(j)){
-            lca = a1.get(i);
-            i--;
-            j--;
+        if(root == p || root == q){
+            return root;
         }
         
-        return new TreeNode(lca);
-    }
-    
-    public ArrayList<Integer> n2rpath(TreeNode root, int data){
-        if(root.val == data){
-            ArrayList<Integer> base = new ArrayList<>();
-            base.add(data);
-            return base;
-        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
         
-        if(root.left!=null){
-            ArrayList<Integer> res = n2rpath(root.left, data);
-            if(res.size()>0){
-                res.add(root.val);
-                return res;
-            }
-        } 
+        if(left!=null && right!=null)
+            return root;
         
-        if(root.right!=null){
-            ArrayList<Integer> res = n2rpath(root.right, data);
-            if(res.size()>0){
-                res.add(root.val);
-                return res;
-            }
-        }
-        
-        return new ArrayList<>();
+        if(left!=null)
+            return left;
+        if(right!=null)
+            return right;
+        return null;
     }
 }
