06e316b3ff6fc4e28e5d1d04c7d878eb212d9c05
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-04-01 21:42:08 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (31.02%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,19 +1,12 @@
 class Solution {
     public int fib(int n) {
         if(n==0 || n==1) return n;
-        //Memoization
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        fibo(n,dp);
-        return dp[n];
-    }
-    
-    public int fibo(int n, int[] dp){
-        if(n==0 || n==1) return n;
+        dp[0] = 0; dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         
-        if(dp[n]!=-1) return dp[n];
-        
-        dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
         return dp[n];
     }
 }
