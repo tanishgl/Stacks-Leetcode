767a83e5ca36c59f16ea79598806732324633396
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-04-12 15:59:40 +0530

    Time: 0 ms (100.00%), Space: 42 MB (18.17%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,10 +1,11 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        mem(nums, 0, dp); 
-        return Math.max(dp[0], dp[1]);
+        // if(nums.length == 1) return nums[0];
+        // int[] dp = new int[nums.length];
+        // Arrays.fill(dp, -1);
+        // mem(nums, 0, dp); 
+        // return Math.max(dp[0], dp[1]);
+        return tab(nums);
     }
     
     public int mem(int[] nums, int house, int[] dp){
@@ -28,5 +29,19 @@ class Solution {
         dp[house] = Math.max(money1, money2);
         
         return dp[house];
+    }
+    
+    public int tab(int[] nums){
+        int[] dp = new int[nums.length];
+        int n = nums.length;
+        dp[n-1] = nums[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+            int no = dp[i+1];
+            int yes = (i+2 < n) ? dp[i+2] + nums[i] : nums[i];
+            dp[i] = Math.max(yes, no);
+        }
+        
+        return dp[0];
     }
 }
