fe01f4659b84d9f61de56660ebf6561eebdb2c7d
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-05-01 11:28:50 +0530

    Time: 19 ms (76.49%), Space: 41.7 MB (95.51%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,7 +2,6 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
         dp[0] = 0;
-        
         for(int i=1;i<=amount;i++){
             for(int j=0;j<coins.length;j++){
                 int need = (i-coins[j]>=0) ? dp[i-coins[j]] : Integer.MAX_VALUE;
@@ -11,6 +10,6 @@ class Solution {
             }
         }
         
-        return (dp[amount] != Integer.MAX_VALUE ) ? dp[amount] : -1;
+        return ( dp[amount] != Integer.MAX_VALUE ) ? dp[amount] : -1;
     }
 }
