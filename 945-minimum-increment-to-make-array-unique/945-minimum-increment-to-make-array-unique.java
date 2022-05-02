b395e871f1f72c1f033ee728f088a1632e805b7f
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-05-02 13:01:23 +0530

    Time: 45 ms (62.20%), Space: 78.8 MB (28.15%) - LeetHub

--- 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.java
+++ 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.java
@@ -1,20 +1,18 @@
 class Solution {
-    private static int SZ = 1000001;
-    
     public int minIncrementForUnique(int[] nums) {
-        int[] arr = new int[SZ];
+        Arrays.sort(nums);
         
-        for(int val : nums){
-            arr[val]++;
-        }
-        
+        int level = -1;
         int count = 0;
-        for(int i=0;i<SZ;i++){
-            if(arr[i]<=1)
-                continue;
-            int extra_freq = arr[i] - 1;
-            arr[i+1] += extra_freq;
-            count += extra_freq;
+        
+        for(int i=0;i<nums.length;i++){
+            int subject = nums[i];
+            if(level < subject){
+                level = subject;
+            } else {
+                level++;
+                count += level - subject;
+            }
         }
         
         return count;
