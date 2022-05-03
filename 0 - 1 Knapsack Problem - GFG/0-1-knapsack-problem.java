03b027c3d5ed83556502ddfb7dac1c85c1c53a9f
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-05-03 22:07:21 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading number of elements and weight
+            int n = Integer.parseInt(read.readLine());
+            int w = Integer.parseInt(read.readLine());
+            
+            int val[] = new int[n];
+            int wt[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            //inserting the values
+            for(int i = 0; i < n; i++)
+              val[i] = Integer.parseInt(st[i]);
+             
+            String s[] = read.readLine().trim().split("\\s+"); 
+            
+            //inserting the weigths
+            for(int i = 0; i < n; i++)
+              wt[i] = Integer.parseInt(s[i]);
+              
+            //calling method knapSack() of class Knapsack
+            System.out.println(new Solution().knapSack(w, wt, val, n));
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        int[] prev = new int[W+1];
+        
+        for(int i=1;i<=n;i++){
+            int[] curr = new int[W+1];
+            for(int j=1;j<=W;j++){
+                int no = prev[j];
+                int yes = (j - wt[i-1] >= 0) ? val[i-1] + prev[j-wt[i-1]] : 0;
+                curr[j] = Math.max(no, yes);
+            }
+            for(int j=0;j<=W;j++){
+                prev[j] = curr[j];
+            }
+        }
+        
+        return prev[W];
+    } 
+}
+
+
