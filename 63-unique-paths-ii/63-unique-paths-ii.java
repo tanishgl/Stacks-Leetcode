0d0edefe3fe119dd52f442484165d5d752484438
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-05-20 09:18:27 +0530

    Time: 1 ms (52.25%), Space: 43.1 MB (5.49%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,25 +1,35 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
+    public int uniquePathsWithObstacles(int[][] grid) {
+        if(grid[grid.length-1][grid[0].length-1] == 1) return 0;
+        if(grid[0][0] == 1) return 0;
+        int n = grid.length;
+        int m = grid[0].length;
         int[][] dp = new int[n][m];
-        
-        if(obstacleGrid[n-1][m-1]==1 || obstacleGrid[0][0] == 1) return 0;
         
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
+                if(grid[i][j] == 1){
+                    dp[i][j] = Integer.MAX_VALUE;
+                    continue;
+                }
+                
                 if(i==n-1 && j==m-1){
                     dp[i][j] = 1;
                     continue;
                 }
                 
-                if(obstacleGrid[i][j]==1){
-                    dp[i][j] = 0;
-                    continue;
-                }
+                int hor = (j + 1 < m) ? dp[i][j+1] : 0;
+                int ver = (i + 1 < n) ? dp[i+1][j] : 0;
                 
-                dp[i][j] = (j+1 < m) ? dp[i][j+1] : 0;
-                dp[i][j] += (i+1 < n) ? dp[i+1][j] : 0;
+                if(hor != Integer.MAX_VALUE && ver != Integer.MAX_VALUE){
+                    dp[i][j] = hor + ver;
+                } else if (hor != Integer.MAX_VALUE){
+                    dp[i][j] = hor;
+                } else if (ver != Integer.MAX_VALUE){
+                    dp[i][j] = ver;
+                } else {
+                    dp[i][j] = 0;
+                }
             }
         }
         
