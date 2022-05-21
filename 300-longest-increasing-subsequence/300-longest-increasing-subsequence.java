cb6c26204eeb1837b622bb34fd4fc9f7f37396e9
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-05-21 10:12:31 +0530

    Time: 72 ms (46.83%), Space: 41.9 MB (82.17%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,25 +1,21 @@
 class Solution {
-    public int memo(int curr, int prev, int[] nums, int[][] dp){
-        if(curr == nums.length) return 0;
-        if(dp[curr][prev + 1] != -1) return dp[curr][prev + 1];
-        
-        int yes = (prev == -1 || nums[prev] < nums[curr])
-                   ? memo(curr + 1, curr, nums, dp) + 1 : 0;
-            
-        int no = memo(curr + 1, prev, nums, dp);
-        
-        return dp[curr][prev + 1] = Math.max(yes, no);
-    }
-    
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=n; j++){
-                dp[i][j] = -1;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        
+        for(int i=1;i<nums.length;i++){
+            for(int j=i-1;j>=0;j--){
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
             }
         }
         
-        return memo(0, -1, nums, dp);
+        int ans = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            ans = Math.max(ans, dp[i]);
+        }
+        
+        return ans;
     }
 }
