f42dbac37e3faf56a05b3ada65cfc13b11e993cc
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-05-24 22:59:32 +0530

    Time: 3 ms (70.23%), Space: 52.5 MB (30.61%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.java
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.java
@@ -1,39 +1,25 @@
 class Solution {
     public int longestMountain(int[] arr) {
         int n = arr.length;
-        // LIS subarray
+        int ans = 0;
         
-        int[] inc = new int[arr.length];
-        inc[0] = 1;
-        int maxL = Integer.MIN_VALUE;
-        for(int i=1;i<n;i++){
-            inc[i] = 1;
-            if(arr[i] > arr[i-1]){
-                inc[i] = inc[i-1] + 1;
-            }
-            maxL = Math.max(maxL, inc[i]);
-        }
-        
-        int[] dec = new int[n];
-        dec[n-1] = 1;
-        int maxR = Integer.MIN_VALUE;
-        for(int i=n-2;i>=0;i--){
-            dec[i] = 1;
-            if(arr[i] > arr[i+1]){
-                dec[i] = dec[i+1] + 1;
-            }
-            maxR = Math.max(maxR, dec[i]);
-        }
-        
-        int ans = Integer.MIN_VALUE;
-        
-        for(int i=1;i<n-1;i++){
-            if(inc[i] >= 2 && dec[i] >= 2){
-                ans = Math.max(ans, inc[i] + dec[i]);
+        for(int i=0;i<n;i++){
+            if(i > 0 && i < n - 1 && arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                int left = i-1, right = i+1, curr = 0;
+                while(left >= 0 && arr[left] < arr[left+1]){
+                    left--;
+                    curr++;
+                }
+                
+                while(right < n && arr[right] < arr[right-1]){
+                    right++;
+                    curr++;
+                }
+                
+                ans = Math.max(ans, curr + 1);
             }
         }
         
-        return (ans == Integer.MIN_VALUE) ? 0 : ans - 1;
-        
+        return ans;
     }
 }
