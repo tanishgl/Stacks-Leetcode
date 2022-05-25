ba23c199af21f9fb90f784d8c67b9d0f0c6ffb38
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-05-25 08:51:07 +0530

    Time: 1 ms (16.17%), Space: 41.7 MB (62.03%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,41 +1,19 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        if(nums.length<3) return 0;
+        if(nums.length < 3) return 0;
         
-        int[] diff = new int[nums.length-1];
-        for(int i=0;i<nums.length-1;i++){
-            diff[i] = nums[i+1] - nums[i];
-        }
+        int[] dp = new int[nums.length];
+        int maxL = 0, currL = 0, count = 0;
         
-        int count = 0;
-        diff = function(diff);
-        
-        for(int i=0;i<diff.length;i++){
-            count += diff[i] - i;
-        }
-        
-        return count;
-    }
-    
-    public int[] function(int[] arr){
-        int[] ans = new int[arr.length];
-        Stack<Integer> st = new Stack<>();
-        st.push(0);
-        
-        for(int i=1;i<arr.length;i++){
-            if(st.size()>0 && arr[i]!=arr[st.peek()]){
-                int idx = st.peek();
-                while(st.size()>0){
-                    ans[st.pop()] = idx;
-                }
+        for(int i=2;i<nums.length;i++){
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]){
+                dp[i] = dp[i-1] + 1;
             }
-            st.push(i);
         }
         
-        int idx = 0;
-        if(st.size()>0) idx = st.peek();
-        while(st.size()>0){
-            ans[st.pop()] = idx;
+        int ans = 0;
+        for(int i=2;i<nums.length;i++){
+            ans += dp[i];
         }
         
         return ans;
