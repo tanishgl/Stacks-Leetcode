e488bc40e52fc0afc0f4cd0e0ef64c7f69fe4341
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-06-11 12:43:39 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (51.53%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,14 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        //using tabulation.
-        int[] dp = new int[n+1];
-        dp[n] = 1;
-        for(int i=n-1;i>=0;i--){
-            dp[i] = dp[i+1];
-            if(i+2 < dp.length)
-                dp[i] += dp[i+2];
+        
+        if(n == 1 || n == 2) return n;
+        
+        int a = 1;
+        int b = 2;
+        
+        for(int i=3;i<=n;i++){
+            b = a + b;
+            a = b - a;
         }
         
-        return dp[0];
+        return b;
     }
 }
