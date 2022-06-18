01b11cbe8691a3a6140f4e7295625af2adbe126f
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-06-18 10:46:02 +0530

    Time: 1 ms (90.61%), Space: 39.9 MB (93.33%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -1,33 +1,21 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> pascal = generate(rowIndex + 1);
-        return pascal.get(pascal.size() - 1);
-    }
-    
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList<>());
-        res.get(0).add(1);
+        List<Integer> prev = new ArrayList<>();
+        prev.add(1);
         
-        int[][] dp = new int[numRows + 1][numRows + 1];
-        for(int i=0;i<dp.length;i++){
-            dp[i][0] = 1;
-        }
-        
-        for(int i=1;i<numRows;i++){
+        for(int i=1;i<=rowIndex;i++){
             List<Integer> row = new ArrayList<>();
-            row.add(1); // nC1
+            row.add(1);
             
             for(int j=1;j<i;j++){
-                // nCk = n-1Ck + n-1Ck-1
-                int nCk = res.get(i-1).get(j) + res.get(i-1).get(j-1);
-                row.add(nCk);
+                row.add(prev.get(j) + prev.get(j-1));
             }
             
-            row.add(1); // nCn
-            res.add(row);
+            row.add(1);
+            
+            prev = row;
         }
         
-        return res;
+        return prev;
     }
 }
