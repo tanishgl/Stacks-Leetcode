c83e39bb16707d1ca5bc430cdf7abf1e92af6f57
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-06-20 11:08:44 +0530

    Time: 27 ms (73.47%), Space: 44.4 MB (76.54%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,40 +1,49 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n = s.length(), m = p.length();
-        Boolean[][] dp = new Boolean[n][m];
-        return memo(0, 0, s, p, dp);
+        int n = s.length() , m = p.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        
+        return helper(0, 0, s, p, dp) == 1;
     }
     
-    public boolean memo(int i, int j, String s, String p, Boolean[][] dp){
-        if(i == s.length() && j == p.length()){
-            return true;
-        } else if (i == s.length()){
-            for(int k=j;k<p.length();k++){
-                if(p.charAt(k) != '*')
-                    return false;
+    public int helper(int i, int j, String s1, String s2, int[][] dp){
+        if(i == s1.length() && j == s2.length())
+            return 1;
+        else if (i == s1.length()){
+            for(int k=j;k<s2.length();k++){
+                if(s2.charAt(k) != '*')
+                    return 0;
             }
-            return true;
-        } else if (j == p.length()){
-            return false;
+            return 1;
         }
+        else if (j == s2.length())
+            return 0;
         
-        if(dp[i][j] != null) return dp[i][j];
         
-        char ch1 = s.charAt(i);
-        char ch2 = p.charAt(j);
+        if(dp[i][j] != -1) return dp[i][j];
         
-        if(ch2 >= 'a' && ch2 <= 'z'){
-            if(ch1 != ch2)
-                return false;
-            return dp[i][j] = memo(i+1, j+1, s, p, dp);
-        } else if (ch2 == '?'){
-            return dp[i][j] = memo(i+1, j+1, s, p, dp);
+        char ch1 = (i < s1.length()) ? s1.charAt(i) : 'A';
+        char ch2 = (j < s2.length()) ? s2.charAt(j) : 'B';
+        
+        if(ch2 == '?'){
+            return dp[i][j] = helper(i + 1, j + 1, s1, s2, dp);
         } else if (ch2 == '*'){
-            boolean no = memo(i, j+1, s, p, dp);
-            boolean yes = memo(i+1, j, s, p, dp);
-            return dp[i][j] = (no || yes);
+            int mul = helper(i, j + 1, s1, s2, dp);
+            int sin = helper(i + 1, j, s1, s2, dp);
+            if(mul == 1 || sin == 1)
+                return dp[i][j] = 1;
+            else 
+                return dp[i][j] = 0;
+        } else {
+            if(ch1 == ch2)
+                return dp[i][j] = helper(i + 1, j + 1, s1, s2, dp);
+            else 
+                return dp[i][j] = 0;
         }
         
-        return true;
+        
     }
 }
