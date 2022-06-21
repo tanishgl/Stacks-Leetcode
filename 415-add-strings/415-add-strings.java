27b520469071342d30215f77c4c095ff56d89a73
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-06-21 18:02:40 +0530

    Time: 36 ms (11.10%), Space: 69.6 MB (9.39%) - LeetHub

---
+++ 415-add-strings/415-add-strings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder ans = new StringBuilder();
+        String str = "";
+        int carry = 0, mul = 1;
+        int i = num1.length() - 1, j = num2.length() - 1;
+        
+        while(i >= 0 || j >= 0 || carry > 0){
+            int d1 = (i>=0) ? num1.charAt(i) - '0' : 0;
+            int d2 = (j>=0) ? num2.charAt(j) - '0' : 0;
+            int sum = d1 + d2 + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            str += sum;
+            i--;
+            j--;
+            mul *= 10;
+        }
+        
+        while(i>=0){
+            str += num1.charAt(i);
+            i--;
+        }
+        
+        while(j>=0){
+            str += num2.charAt(j);
+            j--;
+        }
+        
+        return new StringBuilder(str).reverse().toString();
+    }
+}
