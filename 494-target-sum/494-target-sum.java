1c3238851751d814f23e00f0faffdc4e7e53af50
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-06-22 10:51:35 +0530

    Time: 6 ms (90.97%), Space: 44 MB (38.95%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,28 +1,35 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
-        for(int val : nums)
-            sum += val;
-        
-        if((sum - target) % 2 != 0 || target > sum) return 0;
-        
-        target = (sum - target) / 2;
-        
-        int[] prev = new int[target+1];
-        prev[0] = 1;
-        
-        for(int i=1;i<=nums.length;i++){
-            int[] curr = new int[target+1];
-            for(int j=0;j<=target;j++){
-                int no = prev[j];
-                int yes = (j - nums[i-1] >= 0) ? prev[j - nums[i-1]] : 0;
-                curr[j] = no + yes;
-            }
-            
-            for(int j=0;j<=target;j++)
-                prev[j] = curr[j];
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
         }
         
-        return prev[target];
+        if((sum - target) % 2 != 0 || target > sum) return 0;
+        // System.out.println(target1);
+        
+        return perfectSum(nums, (sum - target) / 2);
     }
+    
+    public int perfectSum(int arr[], int sum) 
+	{ 
+        int n = arr.length;
+	    int[] prev = new int[sum+1];
+	    prev[0] = 1;
+	    
+	    for(int i=1;i<=n;i++){
+	        int[] curr = new int[sum+1];
+	        for(int j=0;j<=sum;j++){
+	            int no = prev[j];
+	            int yes = (j - arr[i-1] >= 0) ? prev[j - arr[i-1]] : 0;
+	            curr[j] = no + yes;
+	        }
+	        
+	        for(int j=0;j<prev.length;j++){
+	            prev[j] = curr[j];
+	        }
+	    }
+	    
+	    return prev[sum];
+	} 
 }
