c1d64b9ee22d20a176642b388c580503f972451b
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-06-22 10:39:53 +0530

    Time: 78 ms (38.51%), Space: 68.4 MB (22.37%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,28 +1,32 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int val : nums)
-            sum += val;
-        if(sum % 2 != 0)
-            return false;
-        
-        int target = sum / 2;
-        
-        boolean[] prev = new boolean[target+1];
-        prev[0] = true;
-        
-        for(int i=1;i<=nums.length;i++){
-            boolean[] curr = new boolean[target+1];
-            for(int j=0;j<=target;j++){
-                boolean no = prev[j];
-                boolean yes = (j - nums[i-1] >= 0) ? prev[j - nums[i-1]] : false;
-                curr[j] = no || yes;
-            }
-            
-            for(int j=0;j<=target;j++)
-                prev[j] = curr[j];
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
         }
         
-        return prev[target];
+        if(sum % 2 != 0) return false;
+        
+        return solve(nums, sum / 2) == 1;
+    }
+    
+    public int solve(int[] A, int B) {
+        int[] prev = new int[B+1];
+        prev[0] = 1;
+        
+        for(int i=1;i<=A.length;i++){
+            int[] curr = new int[B+1];
+            for(int j=0;j<=B;j++){
+                int no = prev[j];
+                int yes = (j - A[i-1] >= 0) ? prev[j - A[i-1]] : 0;
+                curr[j] = (no == 1 || yes == 1) ? 1 : 0;
+            }
+            
+            for(int j=0;j<=B;j++){
+                prev[j] = curr[j];
+            }
+        }
+        
+        return prev[B];
     }
 }
