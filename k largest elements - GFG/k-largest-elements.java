a3a2ae02e696a646feef78b45406158eb4cb31d4
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-06-22 23:02:17 +0530

    Added solution - LeetHub

---
+++ k largest elements - GFG/k-largest-elements.java
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        // code here
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        for(int i=0;i<n;i++){
+            if(minHeap.size() < k){
+                minHeap.add(arr[i]);
+            } else {
+                int lp = minHeap.peek();
+                if(arr[i] > lp){
+                    minHeap.remove();
+                    minHeap.add(arr[i]);
+                }
+            }
+        }
+        
+        int[] op = new int[k];
+        int j = k-1;
+        while(minHeap.size()>0){
+            op[j--] = minHeap.remove();
+        }
+        
+        return op;
+    }
+}
