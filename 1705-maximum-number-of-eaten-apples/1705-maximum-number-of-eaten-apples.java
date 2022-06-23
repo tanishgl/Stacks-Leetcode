69c851516cb56a008ad248b7c6216b76c3338122
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-06-23 23:48:56 +0530

    Time: 3369 ms (5.33%), Space: 118.4 MB (5.92%) - LeetHub

---
+++ 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int eatenApples(int[] apples, int[] days) {
+        PriorityQueue<int[]> carton = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        int count = 0, day = 1;
+        
+        // Iterate as long as carton is not empty or the tree still gives apples.
+        
+        while(day <= days.length || carton.size() > 0){
+            System.out.println(day);
+            // 1. First add today's packet into the carton {apples, expiry}.
+            if(day <= days.length){
+                carton.add(new int[]{apples[day-1], days[day-1] + day});
+                System.out.println("packet added");
+            }
+            
+            // 2. Remove rotten and empty packets.
+            while(carton.size() > 0 && (carton.peek()[0] <= 0 || carton.peek()[1] <= day)){
+                System.out.println("packet removed " + carton.peek()[0] + " " + carton.peek()[1]);
+                carton.remove();
+            }
+            
+            // 3. Time to eat today's apple, if available.
+            if(carton.size() > 0){
+                System.out.println("eaten");
+                count++;
+                int[] packet = carton.remove();
+                carton.add(new int[]{packet[0] - 1, packet[1]});
+            }
+            
+            day++;
+        }
+        
+        return count;
+    }
+}
