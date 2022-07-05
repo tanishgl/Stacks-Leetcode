c1ef214e764d19ab0730cb3746730f044e013bff
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-07-06 00:16:57 +0530

    Time: 4 ms (75.32%), Space: 57.8 MB (26.91%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,23 +1,27 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int ans = 0;
+        int n = grid.length;
+        int m = grid[0].length;
         
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j] == '1'){
+                    count++;
                     DFS(i, j, grid);
-                    ans++;
                 }
             }
         }
         
-        return ans;
+        return count;
     }
     
     public void DFS(int sr, int sc, char[][] grid){
-        if(sr < 0 || sc < 0 || sr >= grid.length || sc >= grid[0].length || grid[sr][sc] == 'N' || grid[sr][sc] == '0') return;
+        if(sr < 0 || sr >= grid.length || sc < 0 || sc >= grid[0].length || grid[sr][sc] == '0' || grid[sr][sc] == 'N')
+            return;
         
         grid[sr][sc] = 'N';
+        
         DFS(sr - 1, sc, grid);
         DFS(sr, sc + 1, grid);
         DFS(sr + 1, sc, grid);
