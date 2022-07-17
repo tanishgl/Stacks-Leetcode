fc1ad39126f23896b044f44c05470961f0c315f5
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-07-17 22:22:59 +0530

    Time: 33 ms (44.77%), Space: 68.4 MB (14.78%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -8,6 +8,7 @@ class Solution {
             this.node = node;
             this.cost = cost;
         }
+        
     }
     
     public int networkDelayTime(int[][] times, int n, int k) {
@@ -17,29 +18,31 @@ class Solution {
         }
         
         for(int[] edge : times){
-            adj[edge[0] - 1].add(new Pair(edge[1] - 1, edge[2]));
+            adj[edge[0]-1].add(new Pair(edge[1]-1, edge[2]));
         }
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.cost - b.cost);
-        pq.add(new Pair(k - 1, 0));
+        PriorityQueue<Pair> q = new PriorityQueue<>((a,b) -> a.cost - b.cost);
+        q.add(new Pair(k-1, 0));
         int[] vis = new int[n];
         Arrays.fill(vis, Integer.MAX_VALUE);
         
-        while(pq.size()>0){
-            Pair front = pq.remove();
-            if(vis[front.node] != Integer.MAX_VALUE) continue;
-            vis[front.node] = front.cost;
+        while(q.size()>0){
+            Pair curr = q.remove();
             
-            for(Pair nbr : adj[front.node]){
-                pq.add(new Pair(nbr.node, nbr.cost + front.cost));
+            if(vis[curr.node] != Integer.MAX_VALUE) continue;
+            vis[curr.node] = curr.cost;
+            
+            for(Pair nbr : adj[curr.node]){
+                q.add(new Pair(nbr.node, curr.cost + nbr.cost));
             }
         }
         
-        int time = vis[k-1];
+        int time = 0;
         for(int i=0;i<n;i++){
+            if(vis[i] == Integer.MAX_VALUE) return -1;
             time = Math.max(time, vis[i]);
         }
         
-        return (time == Integer.MAX_VALUE) ? -1 : time;
+        return time;
     }
 }
