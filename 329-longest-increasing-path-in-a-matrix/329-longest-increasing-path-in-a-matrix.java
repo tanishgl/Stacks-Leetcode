6e9889a458d6d791b4836af5924aa6068e730037
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-07-23 12:33:00 +0530

    Time: 2460 ms (5.01%), Space: 53.8 MB (65.89%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,48 +1,56 @@
-class Solution {
-    
-    public static class Pair{
-        int x, y;
-        Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
+class Pair{
+    int x, y;
+    Pair(int x, int y){
+        this.x = x;
+        this.y = y;
     }
     
-    public static int[] x = {0,1,0,-1};
-    public static int[] y = {1,0,-1,0};
+}
+
+class Solution {
+    
+    public int[] x = {0,0,1,-1};
+    public int[] y = {1,-1,0,0};
     
     public int longestIncreasingPath(int[][] matrix) {
+        
+        // Step 1 :- Prepare the incoming matrix.
         int n = matrix.length, m = matrix[0].length;
-        int[][] inc = new int[n][m];
         Queue<Pair> q = new ArrayDeque<>();
+        int[][] inc = new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 for(int k=0;k<4;k++){
                     int nr = i + x[k];
                     int nc = j + y[k];
-                    if(nr < 0 || nr >= n || nc < 0 || nc >=m || matrix[nr][nc] >= matrix[i][j]) continue;
+                    if(nr < 0 || nc < 0 || nr >= n || nc >= m || matrix[nr][nc] >= matrix[i][j]) continue;
                     inc[i][j]++;
                 }
-                if(inc[i][j] == 0) q.add(new Pair(i,j));
+                if(inc[i][j] == 0)
+                    q.add(new Pair(i,j));
             }
         }
-        
-        Pair[] topo = new Pair[n*m];
+    
+        // Step 3 :- Maintain a visited matrix and initialize array to store topo sort.
+        Pair[] sort = new Pair[n*m];
         int idx = 0;
         
+        // Step 4 :- Apply Kahn's algorithm.
         while(q.size()>0){
-            Pair p = q.remove();
-            topo[idx++] = new Pair(p.x, p.y); 
+            Pair curr = q.remove();
+            sort[idx++] = new Pair(curr.x, curr.y);
+            
             for(int k=0;k<4;k++){
-                int nr = p.x + x[k];
-                int nc = p.y + y[k];
-                if(nr < 0 || nr >= n || nc < 0 || nc >=m || matrix[nr][nc] <= matrix[p.x][p.y]) continue;
+                int nr = curr.x + x[k];
+                int nc = curr.y + y[k];
+                if(nr < 0 || nc < 0 || nr >= n || nc >= m || matrix[nr][nc] <= matrix[curr.x][curr.y]) continue;
                 inc[nr][nc]--;
                 if(inc[nr][nc] == 0)
                     q.add(new Pair(nr, nc));
             }
         }
         
+        // Step 5 :- Figuring out the answer finally.
         int[][] ans = new int[n][m];
         for(int i=0;i<n;i++){
             Arrays.fill(ans[i], 1);
@@ -50,10 +58,10 @@ class Solution {
         
         int res = 1;
         
-        for(int i=topo.length-1;i>=0;i--){
-            int mr = topo[i].x, mc = topo[i].y;
-            for(int j=i+1;j<topo.length;j++){
-                int yr = topo[j].x, yc = topo[j].y;
+        for(int i=sort.length-1;i>=0;i--){
+            int mr = sort[i].x, mc = sort[i].y;
+            for(int j=i+1;j<sort.length;j++){
+                int yr = sort[j].x, yc = sort[j].y;
                 if(matrix[mr][mc] >= matrix[yr][yc])
                     continue;
                 if((Math.abs(yr-mr) == 0 && Math.abs(yc-mc) == 1) || (Math.abs(yr-mr) == 1 && Math.abs(yc-mc) == 0)){
@@ -72,7 +80,5 @@ class Solution {
 //         }
         
         return res;
-        
-        
     }
 }
