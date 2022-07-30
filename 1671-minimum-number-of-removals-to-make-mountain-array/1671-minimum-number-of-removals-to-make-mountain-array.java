b5a59ce6f7579ec16484fde7cc4ef646061bb085
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-07-30 11:57:41 +0530

    Time: 82 ms (34.30%), Space: 46.9 MB (16.26%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
@@ -1,35 +1,32 @@
 class Solution {
     public int minimumMountainRemovals(int[] nums) {
         int n = nums.length;
+        int[] lis = new int[n];
+        int[] rlis = new int[n];
+        Arrays.fill(lis, 1);
+        Arrays.fill(rlis, 1);
         
-        // Calculate LIS
-        int[] inc = new int[n];
-        inc[0] = 1;
-        for(int i=1;i<n;i++){
-            inc[i] = 1;
+        for(int i=0;i<n;i++){
             for(int j=i-1;j>=0;j--){
-                if(nums[j] < nums[i]) inc[i] = Math.max(inc[i], inc[j] + 1);
+                if(nums[i] > nums[j])
+                    lis[i] = Math.max(lis[i], lis[j] + 1);
             }
         }
         
-        // Calculate LDS
-        int[] dec = new int[n];
-        dec[n-1] = 1;
-        for(int i=n-2;i>=0;i--){
-            dec[i] = 1;
+        for(int i=n-1;i>=0;i--){
             for(int j=i+1;j<n;j++){
-                if(nums[j] < nums[i]) dec[i] = Math.max(dec[i], dec[j] + 1);
+                if(nums[i] > nums[j])
+                    rlis[i] = Math.max(rlis[i], rlis[j] + 1);
             }
         }
         
-        int max = Integer.MIN_VALUE;
+        int ans = 0;
         
         for(int i=1;i<n-1;i++){
-            if(inc[i] >= 2 && dec[i] >= 2){
-                max = Math.max(max, inc[i] + dec[i]);
-            }
+            if(lis[i] > 1 && rlis[i] > 1)
+                ans = Math.max(ans, lis[i] + rlis[i] - 1);
         }
         
-        return n - max + 1;
+        return n - ans;
     }
 }
