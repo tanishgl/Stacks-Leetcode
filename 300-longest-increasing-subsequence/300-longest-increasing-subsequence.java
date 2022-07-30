965eb50fb71df3eb8c56609dd4feeb178243bde2
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-07-30 10:49:09 +0530

    Time: 11 ms (76.37%), Space: 44.5 MB (48.49%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,18 +1,34 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
         int longest = 1;
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(nums[0]);
         
-        for(int i=0;i<n;i++){
-            for(int j=i-1;j>=0;j--){
-                if(nums[i] > nums[j])
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] > al.get(al.size()-1))
+                al.add(nums[i]);
+            else {
+                int idx = binarySearch(al, nums[i]);
+                al.set(idx, nums[i]);
             }
-            longest = Math.max(longest, dp[i]);
         }
         
-        return longest;
+        return al.size();
+    }
+    
+    public int binarySearch(ArrayList<Integer> arr, int num){
+        int l = 0, h = arr.size() - 1, ans = arr.size() - 1;
+        
+        while(l <= h){
+            int mid = l + (h - l) / 2;
+            if(arr.get(mid) >= num){
+                ans = mid;
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
+        return ans;
     }
 }
