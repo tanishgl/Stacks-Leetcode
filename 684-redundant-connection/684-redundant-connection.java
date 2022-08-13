4dbb42fd1c18e84de3778e75e90e0027029a8aa8
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-08-13 18:50:29 +0530

    Time: 1 ms (89.49%), Space: 43.9 MB (51.14%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    public static class DSU {
+    class DSU{
         int[] parent, rank;
         
         DSU(int n){
@@ -15,32 +15,30 @@ class Solution {
             return parent[x] = find(parent[x]);
         }
         
-        public void union(int x, int y){
-            int px = find(x);
-            int py = find(y);
-            if(px == py) return;
-            
-            if(rank[px] >= rank[py]){
-                parent[py] = px;
-                rank[px] += rank[py];
+        public void union(int a, int b){
+            a = find(a);
+            b = find(b);
+            if(a == b) return;
+            if(rank[a] >= rank[b]){
+                parent[b] = a;
+                rank[a] += rank[b];
             } else {
-                parent[px] = py;
-                rank[py] += rank[px];
+                parent[a] = b;
+                rank[b] += rank[a];
             }
         }
     }
     
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
-        DSU set = new DSU(n + 1);
+        DSU sets = new DSU(n+1);
         
         for(int[] edge : edges){
-            int src = edge[0], dest = edge[1];
-            if(set.find(src) == set.find(dest)){
+            int a = edge[0];
+            int b = edge[1];
+            if(sets.find(a) == sets.find(b))
                 return edge;
-            }
-            
-            set.union(src, dest);
+            sets.union(a,b);
         }
         
         return null;
