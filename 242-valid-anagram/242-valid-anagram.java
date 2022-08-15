0f76b4ca143c88104390dfdbe069f8ccc98013df
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-08-15 11:42:19 +0530

    Time: 3 ms (94.86%), Space: 42.5 MB (91.58%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -3,18 +3,17 @@ class Solution {
         if(s.length() != t.length()) return false;
         
         int[] a1 = new int[26];
-        int[] a2 = new int[26];
         
         for(int i=0;i<s.length();i++){
             a1[s.charAt(i) - 'a']++;
         }
         
         for(int i=0;i<t.length();i++){
-            a2[t.charAt(i) - 'a']++;
+            a1[t.charAt(i) - 'a']--;
         }
         
         for(int i=0;i<26;i++){
-            if(a1[i] != a2[i])
+            if(a1[i] > 0)
                 return false;
         }
         
