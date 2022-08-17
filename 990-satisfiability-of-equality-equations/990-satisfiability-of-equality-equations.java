c9a06e05ac675e81cccbdb3b75ae9d9670ca3315
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-08-17 09:48:35 +0530

    Time: 2 ms (66.86%), Space: 41.8 MB (88.12%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -1,53 +1,50 @@
 class Solution {
     
-    public static class DSU {
+    static class DSU{
         int[] parent, rank;
         
         DSU(int n){
             parent = new int[n];
-            rank = new int[n];
             Arrays.fill(parent, -1);
+            rank = new int[n];
             Arrays.fill(rank, 1);
         }
         
         public int find(int x){
-            if(parent[x] == -1) return x;
+            if(parent[x] == -1)
+                return x;
             return parent[x] = find(parent[x]);
         }
         
-        public void union(int x, int y){
-            int px = find(x);
-            int py = find(y);
-            if(px == py) return;
-            
-            if(rank[px] >= rank[py]){
-                parent[py] = px;
-                rank[px] += rank[py];
+        public void union(int a, int b){
+            a = find(a);
+            b = find(b);
+            if(a == b) return;
+            if(rank[a] >= rank[b]){
+                parent[b] = a;
+                rank[a] += rank[b];
             } else {
-                parent[px] = py;
-                rank[py] += rank[px];
+                parent[a] = b;
+                rank[b] += rank[a];
             }
         }
     }
     
     public boolean equationsPossible(String[] equations) {
-        DSU sets = new DSU(26);
+        int n = equations.length;
+        DSU set = new DSU(26);
         
         for(String eqn : equations){
-            int left = eqn.charAt(0) - 'a';
-            int right = eqn.charAt(3) - 'a';
             if(eqn.charAt(1) == '='){
-                sets.union(left, right);
+                set.union(eqn.charAt(0)-'a', eqn.charAt(3)-'a');
             }
         }
         
         for(String eqn : equations){
-            int left = eqn.charAt(0) - 'a';
-            int right = eqn.charAt(3) - 'a';
-            if(eqn.charAt(1) == '!'){
-                if(sets.find(left) == sets.find(right))
-                    return false;
-            }
+            if(eqn.charAt(1) == '=')
+                continue;
+            if(set.find(eqn.charAt(0)-'a') == set.find(eqn.charAt(3)-'a'))
+                return false;
         }
         
         return true;
